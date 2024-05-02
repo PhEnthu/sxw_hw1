9468e210ea6eba78030a86689fb709353e8c13a7
Elecflows <653866302@qq.com>
2024-05-03 00:06:22 +0800

    Cmake配置并生成可执行文件（练习2.0）

---
+++ build/CMakeFiles/sxw_hw1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "C:/Users/Elecflows/Desktop/learning_amterials/xlab/xlab_hw/hw1/sxw_hw1/bin/libsxw_hw1.dll.a"
+  "C:/Users/Elecflows/Desktop/learning_amterials/xlab/xlab_hw/hw1/sxw_hw1/bin/sxw_hw1.exe"
+  "C:/Users/Elecflows/Desktop/learning_amterials/xlab/xlab_hw/hw1/sxw_hw1/bin/sxw_hw1.exe.manifest"
+  "C:/Users/Elecflows/Desktop/learning_amterials/xlab/xlab_hw/hw1/sxw_hw1/bin/sxw_hw1.pdb"
+  "CMakeFiles/sxw_hw1.dir/src/algebra.c.obj"
+  "CMakeFiles/sxw_hw1.dir/src/algebra.c.obj.d"
+  "CMakeFiles/sxw_hw1.dir/src/main.c.obj"
+  "CMakeFiles/sxw_hw1.dir/src/main.c.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sxw_hw1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
